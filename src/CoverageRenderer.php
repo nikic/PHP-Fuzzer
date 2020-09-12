929b09c27cab5492b0d2a6cd99b4ce9afdf994db
Nikita Popov <nikita.ppv@gmail.com>
2020-09-12 13:12:49 +0200

    Generate crude coverage overview

--- src/CoverageRenderer.php
+++ src/CoverageRenderer.php
@@ -15,7 +15,10 @@ final class CoverageRenderer {
     public function render(array $fileInfos, array $seenBlocks): void {
         @mkdir($this->outDir);
 
+        $overview = "<table>\n";
+
         $prefix = $this->getCommonPrefix(array_keys($fileInfos));
+        ksort($fileInfos);
         foreach ($fileInfos as $path => $fileInfo) {
             $posToBlockIndex = array_flip($fileInfo->blockIndexToPos);
             ksort($posToBlockIndex);
@@ -23,9 +26,12 @@ final class CoverageRenderer {
             $code = file_get_contents($path);
             $result = '<pre>';
             $lastPos = 0;
+            $numCovered = 0;
+            $numTotal = count($posToBlockIndex);
             foreach ($posToBlockIndex as $pos => $blockIndex) {
                 $result .= htmlspecialchars(\substr($code, $lastPos, $pos - $lastPos));
                 $covered = isset($seenBlocks[$blockIndex]);
+                $numCovered += $covered;
                 $color = $covered ? "green" : "red";
                 $result .= '<span style="background-color: ' . $color . '">' . $code[$pos] . '</span>';
                 $lastPos = $pos + 1;
@@ -37,7 +43,17 @@ final class CoverageRenderer {
             $outPath = $this->outDir . '/' . $shortPath . '.html';
             @mkdir(dirname($outPath), 0777, true);
             file_put_contents($outPath, $result);
+
+            $overview .= <<<HTML
+            <tr>
+                <td><a href="$shortPath.html">$shortPath</a></td>
+                <td>$numCovered/$numTotal</td>
+            </tr>
+            HTML;
         }
+
+        $overview .= '</table>';
+        file_put_contents($this->outDir . '/index.html', $overview);
     }
 
     private function getCommonPrefix(array $strings): string {
